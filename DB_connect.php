63732787f83cfd8ca21b56cb815a0f433c9c3728
Revelations127 <mikensam15@gmail.com>
2024-06-23 06:23:16 -0600

    Delete DB_connect.php

--- DB_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "php_signin_signup", "php_signin_signupbB@", "php_signin_signup");
-
-    if(!$conn){
-        echo "Connection failed to the database!." . mysqli_connect_error();
-    }
-?>
